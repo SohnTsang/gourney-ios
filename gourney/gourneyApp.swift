e4cea096186eb5cddb07c8ac25160019c3eacb36
Tsang Ka Ho <tsangkaho12@gmail.com>
2025-10-13 01:39:27 +0900

    Initial Commit

---
+++ gourney/gourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  gourneyApp.swift
+//  gourney
+//
+//  Created by 曾家浩 on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct gourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gourneyTests/gourneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  gourneyTests.swift
+//  gourneyTests
+//
+//  Created by 曾家浩 on 2025/10/13.
+//
+
+import Testing
+@testable import gourney
+
+struct gourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
